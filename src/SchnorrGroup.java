50fe117675cd18197cba44c67b04ec2df40141f3
Oleksandr Dubenko <odybenko@gmail.com>
2018-12-08 18:07:27 +0100

    Init

---
+++ src/SchnorrGroup.java
@@ -0,0 +1,59 @@
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class SchnorrGroup {
+    public BigInteger p;
+    public BigInteger q;
+    public BigInteger g;
+
+    SchnorrGroup(BigInteger p, BigInteger q, BigInteger g) {
+        this.p = p;
+        this.q = q;
+        this.g = g;
+    }
+
+    /**
+     * Generates p, q, g from Schnorr group
+     *
+     * https://en.wikipedia.org/wiki/Schnorr_group
+     *
+     * @param bitLength
+     */
+    static SchnorrGroup generate(int bitLength) {
+
+        BigInteger TWO = new BigInteger("2");
+
+        SecureRandom sr = new SecureRandom();
+
+        // Generate random q (prime)
+        BigInteger q = new BigInteger(bitLength, 100, sr);
+
+
+        // Generate p such as p = q*r + 1 (prime)
+        BigInteger p;
+        BigInteger r = BigInteger.ONE;
+        while(true) {
+            p = q.multiply(r).add(BigInteger.ONE);
+            if(p.isProbablePrime(100)) break;
+
+            r = r.add(BigInteger.ONE);
+        }
+
+        // Generate g – generator of subgroup
+        BigInteger g, a, ga;
+        while(true) {
+            a = (TWO.add(new BigInteger(bitLength, 100, sr))).mod(p);
+            ga = (p.subtract(BigInteger.ONE)).divide(q);
+            g = a.modPow(ga, p);
+            if(g.compareTo(BigInteger.ONE) != 0) {
+                break;
+            }
+        }
+
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(g);
+
+        return new SchnorrGroup(p,q,g);
+    }
+}
